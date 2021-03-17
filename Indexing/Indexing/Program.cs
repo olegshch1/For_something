4c52621528b15a12ef165b96e5e9a451cb5b59f0
olegshch <st069439@student.spbu.ru>
2021-03-18 00:59:41 +0300

    stemmer is ready

--- Indexing/Indexing/Program.cs
+++ Indexing/Indexing/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Iveonik.Stemmers;
+using System.IO;
 
 namespace Indexing
 {
     class Program
-    {
+    {   
+        
         static void Main(string[] args)
         {
+            var stemmer = new Stemmer();
+            var files = Directory.GetFiles("..\\..\\Texts");
+            foreach (var pathFile in files)
+            {
+                stemmer.StemFile(pathFile);
+            }
+
+            var result = stemmer.GetDict();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var path in item.Value)
+                {
+                    Console.WriteLine(path);
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Indexing/Indexing/Stemmer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexing
+{
+    class Stemmer
+    {
+        private Iveonik.Stemmers.RussianStemmer stem = new Iveonik.Stemmers.RussianStemmer();
+        
+        private Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+        private int counterLine;
+
+        public Stemmer() { }
+
+        public Dictionary<string, List<string>> GetDict()
+        {
+            return dict;
+        }
+
+        public void StemFile(string path)
+        {
+            counterLine = 1;
+            using (var reader = new StreamReader(path))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    var counterWord = 1;
+                    foreach (var res in line.Split(new[] { ' ', ',', '.', '-', ':', '\n' }, StringSplitOptions.RemoveEmptyEntries))
+                    {                        
+                        var ss = stem.Stem(res);
+                        if (!dict.ContainsKey(ss))
+                        {
+                            var local = new List<string>();
+                            local.Add($"path= {path}, string= {counterLine.ToString()}, word= {counterWord.ToString()}");
+                            dict.Add(ss, local);
+                        }
+                        else
+                        {
+                            List<string> value;
+                            dict.TryGetValue(ss, out value);
+                            value.Add($"path= {path}, string= {counterLine.ToString()}, word= {counterWord.ToString()}");
+                        }
+                        counterWord++;
+                    }
+                    counterLine++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
