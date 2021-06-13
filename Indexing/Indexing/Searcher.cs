50affac451934c3b8015911ac8a77a7d3f3bc7ab
olegshch <st069439@student.spbu.ru>
2021-06-13 08:58:31 +0300

    Update Searcher.cs

--- Indexing/Indexing/Searcher.cs
+++ Indexing/Indexing/Searcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -49,6 +50,50 @@ namespace Indexing
             {
                 Find(qstack.Pop());
             }
+        }
+
+        private string Find(string term)
+        {
+            using (var fileStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var buferredStream = new BufferedStream(fileStream))
+            using (var streamReader = new StreamReader(buferredStream))
+            {
+                var line = "";
+                do
+                {
+                    line = streamReader.ReadLine();
+                }
+                while (!line.StartsWith(term) && line.Split(' ')[0] != term);
+                return line;
+            }
+        }
+
+        OrOp(string term1, string term2)
+        {
+            var splitInfo1 = Find(term1).Split(' ');
+            var splitInfo2 = Find(term2).Split(' ');
+            Find(term2);
+            var ind1 = new List<(string, int, int)>();
+            var ind2 = new List<(string, int, int)>();
+            for(int i = 1; i < splitInfo1.Length; i = i + 3)
+            {
+                var path = splitInfo1[i];
+                var lineNumber = Convert.ToInt32(splitInfo1[i + 1]);
+                var wordNumber = Convert.ToInt32(splitInfo1[i + 2]);
+                ind1.Add((path, lineNumber, wordNumber));
+            }
+            for (int i = 1; i < splitInfo2.Length; i = i + 3)
+            {
+                var path = splitInfo2[i];
+                var lineNumber = Convert.ToInt32(splitInfo2[i + 1]);
+                var wordNumber = Convert.ToInt32(splitInfo2[i + 2]);
+                ind2.Add((path, lineNumber, wordNumber));
+            }
+        }
+
+        AndOp(string term1, string term2)
+        {
+
         }
     }
 }
